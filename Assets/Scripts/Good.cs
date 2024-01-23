0c3b46e6893f2d3278dbfb15cb35af05b589a67d
Гнатюк Дмитро <54326535+Pakimonia@users.noreply.github.com>
2024-01-23 22:57:19 +0200

    Add Doods

---
+++ Assets/Scripts/Good.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[Serializable]
+public class Good
+{
+
+    public int Id;
+    public int Level;
+    public Sprite Sprite;
+
+}
---
+++ Assets/Scripts/Good_Repository.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ItemData", menuName = "Goods/GoodsList")]
+public class Good_Repository : ScriptableObject
+{
+    public Good[] goods;  
+    Good[] Provide(int level)
+    {
+        Good[] nededGoods = new Good[30];
+        for (int i = 0,num = 0; i<goods.Length; i++)
+        {
+            if (goods[i].Level == level)
+            {
+                nededGoods[num] = goods[i];
+                num++;
+            }
+        }
+        return nededGoods;
+    }
+
+}
